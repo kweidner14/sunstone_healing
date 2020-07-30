3ad4d53172be3797323d3d346a6f7c982e0eec03
Kyle Weidner <kyle.weidner14@protonmail.com>
2020-07-30 17:22:05 -0600

    fastify

--- app.js
+++ app.js
@@ -22,6 +22,20 @@ const offerings 	= require('./routes/offerings');
 
 const app = express();
 
+import fast from 'fastify'
+const fastify = fast({ logger: true })
+
+fastify.addHook('onRequest', async (request, reply) => {
+  if (request.headers['x-forwarded-proto']) {
+    if (request.headers['x-forwarded-proto'] === 'http') {
+      return reply.redirect(`https://${request.headers.host}${request.raw.url}`)
+    }
+  }
+})
+
+fastify.listen(port, '0.0.0.0');
+
+
 mongoose.connect(process.env.ATLAS_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
